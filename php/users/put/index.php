18dca6a6995393326d9b7e2b3babfc7df55ad4ed
Edgar Perdomo <dotenvsolutions@gmail.com>
2022-03-24 17:16:38 -0500

    Cambios en la generacion del token

--- php/users/put/index.php
+++ php/users/put/index.php
@@ -1,4 +1,9 @@
 <?php
+    date_default_timezone_set('America/Guayaquil');
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    header("Access-Control-Allow-Methods: POST, OPTIONS, PUT");
+    header('Content-Type: application/json');
     require_once('../../helpers/index.php');
 
     $op = $_GET['op'];
@@ -12,22 +17,34 @@
         odbc_autocommit($connect, FALSE); 
         $empresa = $_GET["e"];
         $data =  json_decode(file_get_contents("php://input"));
-        $query = "SELECT * usuario FROM ge_usuarios WHERE usuario = '{$data->usuario}' AND clave = '{$data->pass}' AND empresa = '{$empresa}'";
+        $query = "SELECT * FROM ge_usuarios WHERE usuario = '{$data->user}' AND clave = '{$data->pass}' AND empresa = '{$empresa}'";
         $res = odbc_exec($connect,$query);
         if(odbc_num_rows($res)==0){
             echo json_encode(['success'=>false,'msg'=>'Usuario no encontrado/registrado']);
+            return;
         }else{
             $dt = [];
             while($row = odbc_fetch_array($res)){
                 $row = codificar($row);
                 $dt = $row;
             }
-            if($row['usuario']==$data->usuario && $row['clave']==$data->pass){
-                $getToken = jwt($row['usuario'],$row['email']);
+            //print_r($row);return;
+            if($dt['usuario']==$data->user && $dt['clave']==$data->pass){
+                $getToken = jwt($dt['usuario'],$dt['email']);
                 $generateToken = jwt_encode($getToken,getenv('API_KEY'));
-                echo json_encode(['success'=>true,'data'=>$generateToken]);
+                $query = "UPDATE ge_usuarios SET token_user = '{$generateToken}',token_exp='{$getToken['exp']}' WHERE empresa = '{$empresa}' usuario = '{$dt['usuario']}'";
+                $res = odbc_exec($connect,$query);
+                if(!$res || odbc_error()){
+                    echo json_encode(['success'=>false,'msg'=>'No se ha podido actualizar informacion correctamente']);
+                    return;
+                }else{
+                    odbc_commit($connect);
+                    echo json_encode(['success'=>true,'token'=>$generateToken]);
+                }
             } else {
                 echo json_encode(['success'=>false,'msg'=>'Usuario o clave incorrectos']);
+                return;
             }
         }
+        odbc_close($connect);
     }
