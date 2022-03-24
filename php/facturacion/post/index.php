16a9e1c2103dbd97c2811fd65fc8fa29a75fd38a
Edgar Perdomo <dotenvsolutions@gmail.com>
2022-03-24 17:57:35 -0500

    Creando Funcionalidad de cabeceras

---
+++ php/facturacion/post/index.php
@@ -0,0 +1,23 @@
+<?php
+
+    date_default_timezone_set('America/Guayaquil');
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    header("Access-Control-Allow-Methods: POST, OPTIONS");
+    header('Content-Type: application/json');
+    require_once('../../helpers/index.php');
+
+    $op = $_GET['op'];
+    switch($op){
+        case "createDocument":createDocument();break;
+        default: "No ha especificado una ruta";break;
+    }
+
+    function createDocument(){
+        require_once '../../../connectdb.php';
+        odbc_autocommit($connect, FALSE); 
+        $empresa = $_GET["e"];
+        $data =  json_decode(file_get_contents("php://input"));
+        $headers = apache_request_headers();
+        print_r($headers);return;
+    }
